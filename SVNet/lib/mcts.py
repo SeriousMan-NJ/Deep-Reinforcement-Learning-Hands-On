6d7575ece79735dee0baf885a56e417ee7f60b98
Yongwon Shin <ywshin@postech.ac.kr>
2021-02-18 10:53:05 +0900

    profile

--- SVNet/lib/mcts.py
+++ SVNet/lib/mcts.py
@@ -77,10 +77,7 @@ class MCTS:
             # convert
             action = model.convert_action(cur_state, action)
 
-            start = time.time()
             cur_state, spill_costs = allocation.move(cur_state, action)
-            end = time.time()
-            print("move: {}".format(end - start))
             cur_state_int = allocation.state_to_int(cur_state)
 
             if spill_costs >= 0:
